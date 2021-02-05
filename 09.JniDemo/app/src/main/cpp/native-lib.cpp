166c7f285e2336f20eabea96ab92db1ceceff308
WangJin-9909 <990912517@qq.com>
2021-02-05 16:48:10 +0800

    20210205

--- 09.JniDemo/app/src/main/cpp/native-lib.cpp
+++ 09.JniDemo/app/src/main/cpp/native-lib.cpp
@@ -14,10 +14,28 @@ Java_com_example_jnidemo_MainActivity_test(
         JNIEnv *env,
         jobject /* this */) {
     //2020.12.11空指针测试
-    char *ret = NULL;
+    char *ret = (char *)malloc(4);
+
+    char **m ;
+    m = (char**)malloc(sizeof(char *) * 100000); //行数
+
+    long i=0;
+    for( i=0;i<1000000000000 ;i++ )
+    {
+        m[i]= (char*)malloc(sizeof(char)*1000); //为每行申请空间
+
+    }
+    strcpy( m[0], "hello" );
+    m[0][0]= 'H' ;
+
+
+
+
+
+    //__android_log_print( ANDROID_LOG_DEBUG, "+++++++++++++++++++++++++++++++++++++++++++++++WANGJIN","sizeof(ret) = %d", sizeof(ret));
     try{
 
-        *ret = 1;
+       // *ret = 1;
     }catch (...){
         __android_log_print( ANDROID_LOG_DEBUG, "WANGJIN","----FAIL---");
     }
